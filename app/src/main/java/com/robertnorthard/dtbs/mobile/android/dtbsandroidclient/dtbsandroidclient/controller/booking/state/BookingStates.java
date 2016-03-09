b20f97882882f391a8d3679dfbdb069f93a1dad7
Robert Northard <robertnorthard@googlemail.com>
2016-03-09 12:26:47 +0000

    Removed booking states class

--- app/src/main/java/com/robertnorthard/dtbs/mobile/android/dtbsandroidclient/dtbsandroidclient/controller/booking/state/BookingStates.java
+++
@@ -1,8 +0,0 @@
-package com.robertnorthard.dtbs.mobile.android.dtbsandroidclient.dtbsandroidclient.controller.booking.state;
-
-/**
- * Created by robertnorthard on 20/02/16.
- */
-public enum BookingStates {
-    AWAITING_TAXI
-}
